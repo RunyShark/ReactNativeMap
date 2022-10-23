415c64e0b3890ca8447611cc66d0fe0244fff407
RunyShark <spartadragon117@live.com.mx>
2022-10-22 23:39:10 -0500

    requestPermissions

--- src/screen/Permissions/index.tsx
+++ src/screen/Permissions/index.tsx
@@ -1,10 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Platform, Text, View} from 'react-native';
+import {
+  check,
+  request,
+  PERMISSIONS,
+  PermissionStatus,
+} from 'react-native-permissions';
+
+import {styles} from './style';
 
 export const PermissionsScreen = () => {
+  const {ANDROID} = PERMISSIONS;
+
+  let permissionStatus: PermissionStatus;
+
+  const checkLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      permissionStatus = await request(ANDROID.ACCESS_FINE_LOCATION);
+    }
+    console.log({permissionStatus});
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Hello word</Text>
+      <Button title="Permission" onPress={checkLocationPermission} />
     </View>
   );
 };
